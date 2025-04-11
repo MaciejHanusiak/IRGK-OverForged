b88cac105489479b99f44039b2a93bc584ba7224
mhanusiak <maciej.hanusiak7@gmail.com>
2025-04-11 02:03:46 +0200

    Update PlayerMovementScript with support for Player movement animations

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,10 +1,21 @@
+using System;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
     Vector2 dir;
     [SerializeField] float moveSpeed = 5f;
-    
+    Animator animator;
+
+    float moveX, moveY;
+
+    float moveStance;
+    int direction;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
     void Start()
     {
         
@@ -13,9 +24,32 @@ public class PlayerMovement : MonoBehaviour
     
     void Update()
     {
-        dir.x = Input.GetAxisRaw("Horizontal");
-        dir.y = Input.GetAxisRaw("Vertical");
+        // Get player Input
+        moveX = Input.GetAxisRaw("Horizontal");
+        moveY = Input.GetAxisRaw("Vertical");
+        
+        // set player vector
+        dir.x = moveX;
+        dir.y = moveY;
         dir.Normalize();
+
+        moveStance = Mathf.Abs(dir.x) + Mathf.Abs(dir.y);
+        animator.SetFloat("SpeedFloat", moveStance);
+
+
+
+        if (dir.y < 0)      // Down
+                direction = 0;
+        else if (dir.x < 0) // Left
+                direction = 1;
+        else if (dir.y > 0) // Up
+                direction = 2;
+        else if (dir.x > 0) // Right
+                direction = 3;
+
+        animator.SetInteger("DirectionInt", direction);
+
         transform.Translate(Time.deltaTime * dir * moveSpeed);
+        Debug.Log("MoveStance:" + moveStance + " Direction:" + direction + " " +  dir);
     }
 }
