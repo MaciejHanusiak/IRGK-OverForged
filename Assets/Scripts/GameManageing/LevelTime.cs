e8135c9116873ed5d9cf8a4c83d982304ec336aa
mhanusiak <maciej.hanusiak7@gmail.com>
2025-04-11 02:49:19 +0200

    Add UI panel and simple level ending timer

---
+++ Assets/Scripts/GameManageing/LevelTime.cs
@@ -0,0 +1,58 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+public class LevelTime : MonoBehaviour
+{
+    public float timeRemaining = 60f;
+    public float maxTime = 60f;
+    private bool timerIsRunning = false;
+
+    [SerializeField] TextMeshProUGUI timerText;
+    [SerializeField] Slider timerSlider;
+    
+    void Start()
+    {
+        timerIsRunning = true;
+
+        if (timerSlider != null)
+        {
+            timerSlider.maxValue = maxTime;
+            timerSlider.value = timeRemaining;
+        }
+        UpdateTimerUI();
+    }
+
+    
+    void Update()
+    {
+        if (timerIsRunning)
+        {
+            if (timeRemaining > 0)
+            {
+                timeRemaining -= Time.deltaTime;
+                UpdateTimerUI();
+            }
+            else
+            {
+                timeRemaining = 0;
+                timerIsRunning = false;
+                UpdateTimerUI();
+                Debug.Log("Koniec gry");
+            }
+        }
+    }
+
+    void UpdateTimerUI()
+    {
+        if (timerText != null)
+        {
+            int seconds = Mathf.FloorToInt(timeRemaining);
+            int milliseconds = Mathf.FloorToInt((timeRemaining - seconds) * 1000);
+            timerText.text = string.Format("{0}:{1:000}", seconds, milliseconds);
+        }
+        if (timerSlider != null)
+        {
+            timerSlider.value = timeRemaining;
+        }
+    }
+}
