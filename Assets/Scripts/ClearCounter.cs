74d7ef905f8aca1e032a4809b2997f8052f54311
mhanusiak <maciej.hanusiak7@gmail.com>
2025-04-25 23:09:20 +0200

    Create first prefabs of smith objects and instantiate that object when player make an interaction with clear counter

--- Assets/Scripts/ClearCounter.cs
+++ Assets/Scripts/ClearCounter.cs
@@ -2,8 +2,11 @@ using UnityEngine;
 
 public class ClearCounter : MonoBehaviour
 {
+    [SerializeField] private Transform orePrefab;
+    [SerializeField] private Transform counterTopPoint;
     public void Interact()
     {
         Debug.Log("Interact");
+        Instantiate(orePrefab, counterTopPoint);
     }
 }
