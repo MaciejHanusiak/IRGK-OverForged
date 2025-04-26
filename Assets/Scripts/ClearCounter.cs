8ead41a4f31601001c5736f3c22d971704a6b3a8
mhanusiak <maciej.hanusiak7@gmail.com>
2025-04-26 13:31:53 +0200

    Add setting a smith object to clear counter and setting clear counter to smith object, make a possible to identify where they are setted.

--- Assets/Scripts/ClearCounter.cs
+++ Assets/Scripts/ClearCounter.cs
@@ -9,10 +9,16 @@ public class ClearCounter : MonoBehaviour
     public void Interact()
     {
         if (smithObject == null)
-        {
+        {   
+            // Create new smith object if none exist
             Transform smithObjectTransform = Instantiate(smithObjectSO.prefab, counterTopPoint);
             smithObjectTransform.localPosition = Vector2.zero;
-            smithObject = smithObjectTransform.GetComponent<SmithObject>();
+            smithObject = smithObjectTransform.GetComponent<SmithObject>(); // Set Smith Object to Clear Counter
+            smithObject.SetClearCounter(this); // Set Clear Counter to Smith Object.
+        }
+        else
+        {
+            Debug.Log(smithObject.GetClearCounter());
         }
         
     }
--- Assets/Scripts/SmithObject.cs
+++ Assets/Scripts/SmithObject.cs
@@ -4,8 +4,19 @@ public class SmithObject : MonoBehaviour
 {
     [SerializeField] private SmithObjectSO smithObjectSO;
 
+    private ClearCounter clearCounter;
     public SmithObjectSO GetSmithObjectSO()
     {
         return smithObjectSO;
+    }
+
+    public void SetClearCounter(ClearCounter clearCounter)
+    {
+        this.clearCounter = clearCounter;
+    }
+
+    public ClearCounter GetClearCounter()
+    {
+        return clearCounter;
     }
 }
