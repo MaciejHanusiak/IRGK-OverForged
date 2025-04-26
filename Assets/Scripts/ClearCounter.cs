38b3b62c0ab344c6e53a2b248dfe22f1cc618da3
mhanusiak <maciej.hanusiak7@gmail.com>
2025-04-26 16:48:50 +0200

    Add visual change for smith object when player interact with it using testing 'T' key, change of smith object property that clear counter has.

--- Assets/Scripts/ClearCounter.cs
+++ Assets/Scripts/ClearCounter.cs
@@ -4,22 +4,59 @@ public class ClearCounter : MonoBehaviour
 {
     [SerializeField] private SmithObjectSO smithObjectSO;
     [SerializeField] private Transform counterTopPoint;
+    [SerializeField] private ClearCounter secondClearCounter;
+
+    public bool testing;
 
     private SmithObject smithObject;
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.T) && testing)
+        {
+            if (smithObject != null)
+            {
+                smithObject.SetClearCounter(secondClearCounter);
+            }
+            else
+            {
+                Debug.LogError("Clear counter does't has any object!");
+            }
+        }
+    }
     public void Interact()
     {
+
         if (smithObject == null)
-        {   
+        {
             // Create new smith object if none exist
             Transform smithObjectTransform = Instantiate(smithObjectSO.prefab, counterTopPoint);
+            smithObjectTransform.GetComponent<SmithObject>().SetClearCounter(this);
             smithObjectTransform.localPosition = Vector2.zero;
-            smithObject = smithObjectTransform.GetComponent<SmithObject>(); // Set Smith Object to Clear Counter
-            smithObject.SetClearCounter(this); // Set Clear Counter to Smith Object.
         }
         else
         {
-            Debug.Log(smithObject.GetClearCounter());
+            Debug.LogError(smithObject.GetClearCounter() + " already has a object on it!");
         }
         
+    }
+    public Transform GetSmithObjectFollowTransform()
+    {
+        return counterTopPoint;
+    }
+    public void SetSmithObject(SmithObject smithObject)
+    {
+        this.smithObject = smithObject;
+    }
+    public SmithObject GetSmithObject()
+    {
+        return smithObject;
+    }
+    public void ClearSmithObject()
+    {
+        smithObject = null;
+    }
+    public bool HasSmithObject()
+    {
+        return smithObject != null;
     }
 }
--- Assets/Scripts/SmithObject.cs
+++ Assets/Scripts/SmithObject.cs
@@ -12,7 +12,19 @@ public class SmithObject : MonoBehaviour
 
     public void SetClearCounter(ClearCounter clearCounter)
     {
+        if (this.clearCounter != null)
+        {
+            this.clearCounter.ClearSmithObject();
+        }
         this.clearCounter = clearCounter;
+
+        if (clearCounter.HasSmithObject())
+        {
+            Debug.LogError("Counter already has a SmithObject!");
+        }
+        clearCounter.SetSmithObject(this);
+        transform.parent = clearCounter.GetSmithObjectFollowTransform();
+        transform.localPosition = Vector2.zero;
     }
 
     public ClearCounter GetClearCounter()
