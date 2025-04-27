ba5a530de894401b43bf12428b6eae6c874157d8
mhanusiak <maciej.hanusiak7@gmail.com>
2025-04-27 14:36:34 +0200

    Add pick up and drop down logic for player using clear counter with and without smith object in the hand.

--- Assets/Scripts/ClearCounter.cs
+++ Assets/Scripts/ClearCounter.cs
@@ -3,11 +3,38 @@ using UnityEngine;
 public class ClearCounter : BaseCounter
 {
     [SerializeField] private SmithObjectSO smithObjectSO;
-    
 
- 
-    //public override void Interact(Player player)
-    //{
 
-    //}
+
+    public override void Interact(Player player)
+    {
+        if (!HasSmithObject())
+        {
+            // There is no smith object
+
+            if (player.HasSmithObject())
+            {
+                // Player is carrying something
+                player.GetSmithObject().SetSmithObjectParent(this);
+            }
+            else
+            {
+                // Player is not carrying anything
+            }
+        }
+        else
+        {
+            // There is smith object
+
+            if (player.HasSmithObject())
+            {
+                // Player carrying an object
+            }
+            else
+            {
+                // Player is no carrying anything
+                this.GetSmithObject().SetSmithObjectParent(player);
+            }
+        }
+    }
 }
