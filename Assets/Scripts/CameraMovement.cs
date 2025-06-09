4583811a5da6f7c9812010a21b30e4c333ba4baf
mhanusiak <maciej.hanusiak7@gmail.com>
2025-06-09 15:33:57 +0200

    Add camera follow player script

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    [SerializeField] private Transform player;
+    [SerializeField] private Vector3 offset;
+    [SerializeField] private float speed;  
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 desiredPos = player.position + offset;
+        transform.position = Vector3.Lerp(transform.position, desiredPos, speed * Time.deltaTime);
+    }
+}
