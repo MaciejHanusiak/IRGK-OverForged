bc0bf19bca535907bfac654408aab9701641d64b
mhanusiak <maciej.hanusiak7@gmail.com>
2025-06-18 22:32:34 +0200

    Add weapon stand script, prefab and SO

--- Assets/Scripts/Counters/WorkBenchCounter.cs
+++ Assets/Scripts/Counters/WorkBenchCounter.cs
@@ -1,6 +1,29 @@
+using System;
 using UnityEngine;
 
 public class WorkBenchCounter : BaseCounter
 {
-  
+    [SerializeField] private SmithObjectSO weaponStandObjectSO;
+    public override void Interact(Player player)
+    {
+        if (!HasSmithObject())
+        {
+            // There is no smith object, player has or hasn't something.
+            SmithObject.SpawnSmithObject(weaponStandObjectSO, this);
+        }
+        else
+        {
+            // There is smith object
+
+            if (player.HasSmithObject())
+            {
+                // Player carrying an object
+            }
+            else
+            {
+                // Player is no carrying anything
+                player.GetSmithObject().SetSmithObjectParent(this);
+            }
+        }
+    }
 }
