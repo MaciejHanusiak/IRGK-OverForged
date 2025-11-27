70fc611371387530a6e6b0249101e64c2667ed28
mhanusiak <maciej.hanusiak7@gmail.com>
2025-11-27 22:04:24 +0100

    Add level pause when contidion is met

--- Assets/Scripts/GameManageing/CurrentLevelManager.cs
+++ Assets/Scripts/GameManageing/CurrentLevelManager.cs
@@ -3,21 +3,34 @@ using UnityEngine;
 public class CurrentLevelManager : MonoBehaviour
 {
     [SerializeField] public int levelGoldGoal;
-    string LevelEndStatement { get; set; }
+    private string LevelEndStatement = string.Empty;
+    private float normalFixedDeltaTime;
 
-    void Start()
+    private bool hasLevelEnded = false;
+
+    private void Awake()
     {
-
+        normalFixedDeltaTime = Time.fixedDeltaTime; // Save default value
     }
 
-    // Update is called once per frame
     void Update()
     {
+        // If level ended do nothing
+        if (hasLevelEnded) return;
         LevelEndStatement = LevelStats.Instance.gold >= levelGoldGoal ? "Congrats! You won this lvl!" : 
             LevelTime.timeRemaining <= 0 ? "Game over! :<" : "";
-        Debug.Log(LevelEndStatement);
+        if (LevelEndStatement != "")
+        {
+            ShowLevelEndScreen(LevelEndStatement);
+            hasLevelEnded = true; // lvl ended, stop level
+        }
     }
 
+    public void ShowLevelEndScreen(string lvlEndStmt)
+    {
+        Debug.Log(lvlEndStmt);
+        Time.timeScale = 0f;
+        Time.fixedDeltaTime = normalFixedDeltaTime * Time.timeScale;
+    }
 
-   
 }
