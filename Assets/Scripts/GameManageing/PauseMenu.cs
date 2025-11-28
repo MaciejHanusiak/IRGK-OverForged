fe7760847ffa3057521efbd86a463fbfa644a639
mhanusiak <maciej.hanusiak7@gmail.com>
2025-11-28 03:05:50 +0100

    Add time pause while game paus

--- Assets/Scripts/GameManageing/PauseMenu.cs
+++ Assets/Scripts/GameManageing/PauseMenu.cs
@@ -8,17 +8,21 @@ public class PauseMenu : MonoBehaviour
     public void Pause()
     {
         pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
     }
     public void Home()
     {
         SceneManager.LoadScene("Main Menu");
+        Time.timeScale = 1f;
     }
     public void Resume()
     {
         pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
     }
     public void Restart()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        Time.timeScale = 1f;
     }
 }
