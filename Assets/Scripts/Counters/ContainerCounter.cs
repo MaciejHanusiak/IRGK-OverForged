d806c0e411c43ff6c078cd7d6c07244e56827945
mhanusiak <maciej.hanusiak7@gmail.com>
2025-12-27 13:04:27 +0100

    Delete unnecessary price logic from ContainerCounter.cs

--- Assets/Scripts/Counters/ContainerCounter.cs
+++ Assets/Scripts/Counters/ContainerCounter.cs
@@ -3,28 +3,28 @@ using UnityEngine;
 public class ContainerCounter : BaseCounter
 {
     [SerializeField] public SmithObjectSO smithObjectSO;
-    private int priceForMaterial = 0;
+    //private int priceForMaterial = 0;
 
 
     public override void Interact(Player player)
     {
         if (!player.HasSmithObject())
         {
-            // Player is not carrying anything
-            switch (smithObjectSO.name)
-            {
-                case "WoodenLog":
-                    priceForMaterial = 1;
-                    break;
-                case "CopperOre":
-                    priceForMaterial = 2;
-                    break;
-                case "IronOre":
-                    priceForMaterial = 3;
-                    break;
-            }
+            //// Player is not carrying anything
+            //switch (smithObjectSO.name)
+            //{
+            //    case "WoodenLog":
+            //        priceForMaterial = 1;
+            //        break;
+            //    case "CopperOre":
+            //        priceForMaterial = 2;
+            //        break;
+            //    case "IronOre":
+            //        priceForMaterial = 3;
+            //        break;
+            //}
 
-            if (LevelStats.Instance.SpendGold(priceForMaterial))
+            if (LevelStats.Instance.SpendGold(smithObjectSO.price))
             {
                 SmithObject.SpawnSmithObject(smithObjectSO, player);
                 Analytics.Instance.PlayerBuySomething(smithObjectSO.name, LevelStats.Instance.gold, LevelTime.Instance.timeRemaining);
