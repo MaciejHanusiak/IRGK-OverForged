2db51fd5c526784130dbd1a4891e67b85bfdd376
mhanusiak <maciej.hanusiak7@gmail.com>
2025-12-30 22:55:53 +0100

    Add hint script

--- Assets/Scripts/UI/TutorialUI/HintPanel.cs
+++ Assets/Scripts/UI/TutorialUI/HintPanel.cs
@@ -2,15 +2,18 @@ using UnityEngine;
 
 public class HintPanel : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        Time.timeScale = 0f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Return) || Input.GetKeyDown(KeyCode.Space))
+        {
+            Time.timeScale = 1f;
+            this.gameObject.SetActive(false);
+        }
     }
 }
